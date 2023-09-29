e40ae723c668ec6469d18bcb7d29fb4293c232a7
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-09-29 15:50:35 +0200

    add svg for star rating

--- src/App.vue
+++ src/App.vue
@@ -40,9 +40,13 @@ export default {
           <div>TITOLO ORIGINALE: {{ movie.original_title }}</div>
           <div>
             <span>VOTO: {{ Math.round(movie.vote_average / 2) }}</span>
-            <span>
-              <i class="fa-solid fa-star" v-for="n in Math.round(movie.vote_average / 2)"></i>
-
+            <span v-for="n in Math.round(movie.vote_average / 2)">
+            
+              
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                <path
+                  d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+              </svg>
             </span>
           </div>
           <div>LINGUA: {{ movie.original_language.toUpperCase() }} <img width="30"
@@ -65,7 +69,14 @@ export default {
           <div>TITOLO ORIGINALE: {{ serie.original_name }}</div>
           <div>
             <span>VOTO: {{ Math.round(serie.vote_average / 2) }}</span>
-            <span><i class="fa-solid fa-star" v-for="n in Math.round(serie.vote_average / 2)"></i></span>
+            <span v-for="n in Math.round(movie.vote_average / 2)">
+            
+              
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+              <path
+                d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+            </svg>
+          </span>
           </div>
           <div>LINGUA: {{ serie.original_language.toUpperCase() }} <img width="30"
               :src="'https://purecatamphetamine.github.io/country-flag-icons/3x2/' + serie.original_language.toUpperCase() + '.svg'"
